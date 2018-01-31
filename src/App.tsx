1d7891102bc5bca6e43b387c2e9f61abefe712ee
rick batka <rick.batka@gmail.com>
2018-01-31 17:51:56 -0500

    bring in mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import './App.css';
+import { observable } from "mobx"
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+  @observable name: string = 'asd';
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React, {name}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          <input name="yourname" value="" onChange=TODORICK/>
         </p>
       </div>
     );
