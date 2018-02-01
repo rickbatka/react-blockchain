3b09a3c1e95ccd88edc374087b971b360347602e
rick batka <rick.batka@gmail.com>
2018-02-01 10:45:58 -0500

    hello world with mobx working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import './App.css';
-import { observable } from "mobx"
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { ChangeEvent } from 'react';
 
 const logo = require('./logo.svg');
 
+@observer
 class App extends React.Component {
   @observable name: string = 'asd';
+
+  constructor(props: any) {
+    super(props);
+  }
+  
+  public nameChanged = (e: ChangeEvent<HTMLInputElement>) => {
+    this.name = e.target.value;
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,8 +26,9 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React, {name}</h1>
         </header>
         <p className="App-intro">
-          <input name="yourname" value="" onChange=TODORICK/>
+          <input name="yourname" value={this.name} onChange={this.nameChanged} />
         </p>
+        <h3>here it is: {this.name} </h3>
       </div>
     );
   }
