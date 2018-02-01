171af5667c3677a9376e40e88f1ac9a42d3fdf13
rick batka <rick.batka@gmail.com>
2018-02-01 11:44:44 -0500

    bring in bulma css, lay out rough development UI view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { ChangeEvent } from 'react';
@@ -13,7 +14,7 @@ class App extends React.Component {
   constructor(props: any) {
     super(props);
   }
-  
+
   public nameChanged = (e: ChangeEvent<HTMLInputElement>) => {
     this.name = e.target.value;
   }
@@ -25,10 +26,25 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React, {name}</h1>
         </header>
-        <p className="App-intro">
-          <input name="yourname" value={this.name} onChange={this.nameChanged} />
-        </p>
-        <h3>here it is: {this.name} </h3>
+        
+        <div className="columns is-marginless" id="main-content">
+          <div className="column left-sidebar">
+            <h1>
+              Nodes
+            </h1>
+          </div>
+          <div className="column middle is-three-fifths">
+            <h1>Blocks</h1>
+          </div>
+          <div className="column right-sidebar">
+            <h1>
+              Miners
+            </h1>
+          </div>
+        </div>
+        <div id="footer-content">
+          <h1>Output</h1>
+        </div>
       </div>
     );
   }
